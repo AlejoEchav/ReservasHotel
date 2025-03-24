bea9c896ec0b46a3952f3af9ff3b5c543a2d932c
AlejoEchav <alejoechavarria123@gmail.com>
2025-03-24 12:56:57 -0500

    Se agrega la clase HabitacionEstandar la cual hereda de Reserva.

---
+++ ReservasHotel/HabitacionEstandar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservasHotel
+{
+    public class HabitacionEstandar : Reserva
+    {
+        public double TarifaFija { get; set; }
+
+        public HabitacionEstandar(string nombreCliente, int numeroHabitacion, DateTime fechaReserva, int duracionEstadia, double tarifaFija)
+            : base(nombreCliente, numeroHabitacion, fechaReserva, duracionEstadia)
+        {
+            if (tarifaFija <= 0)
+                throw new ArgumentException("La tarifa debe ser mayor a 0.");
+
+            TarifaFija = tarifaFija;
+        }
+
+        public override double CalcularCostoTotal()
+        {
+            return TarifaFija * DuracionEstadia;
+        }
+    }
+}
