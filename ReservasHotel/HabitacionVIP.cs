03d862b4c1660433043740d1777bd3e356f252aa
AlejoEchav <alejoechavarria123@gmail.com>
2025-03-24 14:07:53 -0500

    Se incluye la sobrecarga de motodos.

--- ReservasHotel/HabitacionVIP.cs
+++ ReservasHotel/HabitacionVIP.cs
@@ -8,26 +8,31 @@ namespace ReservasHotel
 {
     public class HabitacionVIP : Reserva
     {
-        public double TarifaBase { get; set; }
+        public double TarifaPorNoche { get; set; }
         private const double DescuentoVIP = 0.20;
 
-        public HabitacionVIP(string nombreCliente, int numeroHabitacion, DateTime fechaReserva, int duracionEstadia, double tarifaBase)
+        public HabitacionVIP(string nombreCliente, int numeroHabitacion, DateTime fechaReserva, int duracionEstadia, double tarifaPorNoche)
             : base(nombreCliente, numeroHabitacion, fechaReserva, duracionEstadia)
         {
-            if (tarifaBase <= 0)
+            if (tarifaPorNoche <= 0)
                 throw new ArgumentException("La tarifa debe ser mayor a 0.");
 
-            TarifaBase = tarifaBase;
+            TarifaPorNoche = tarifaPorNoche;
         }
 
         public override double CalcularCostoTotal()
         {
-            double total = TarifaBase * DuracionEstadia;
+            double total = TarifaPorNoche * DuracionEstadia;
 
             if (DuracionEstadia > 5)
                 total -= total * DescuentoVIP;
 
             return total;
+        }
+
+        public new double CalcularCostoTotal(int noches)
+        {
+            return base.CalcularCostoTotal(noches, TarifaPorNoche);
         }
     }
 }
